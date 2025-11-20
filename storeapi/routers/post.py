8b3d2d1ab88ea6624852c5574bfc269a47bcfa1e
mogesTesema <moges2421@gmail.com>
2025-11-20 22:05:24 +0300

    error fexing due to absence of / in /post/post_id/comment endpoint

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -49,8 +49,19 @@ async def get_posts():
     return list(post_table.values())
 
 
-@router.get("post/{post_id}/comment",response_model=list[Comment])
-async def get_comments_on_post(post_id:int):
+@router.get("/post/{post_id}/comment",response_model=list[Comment])
+def get_comments_on_post(post_id:int):
     return [
         comment for comment in  comment_table.values() if comment["post_id"]==post_id
     ]
+
+@router.get("/post/{post_id}",response_model=UserPostWithComments)
+async def get_post_with_comments(post_id:int):
+    post = find_post(post_id=post_id)
+    if not post:
+        raise HTTPException(status_code=404,detail="comment not found")
+    
+    return {
+        "post":post,
+        "comment": get_comments_on_post(post_id)
+    }
