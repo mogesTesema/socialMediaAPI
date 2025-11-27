e4d6fa8fad7790578ac53692dd468e8119fd3358
mogesTesema <moges2421@gmail.com>
2025-11-28 01:49:56 +0300

    logging configuration using logging.config dictConfig method and rich

--- storeapi/main.py
+++ storeapi/main.py
@@ -4,13 +4,18 @@ from storeapi.routers.post import router as post_router
 from storeapi.fileuploads.fileuploaderouters import router as upload_router
 from storeapi.videochats.videochatrouter import router as videochat_router
 from storeapi.database import database
+from storeapi.logging_config import configure_logging
+import logging
 
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # ---- SETUP (start server) ----
     print("📡 Connecting database...")
     await database.connect()
+    configure_logging()
+    logger.info("hello loger")
 
     yield  # ⛔ Pause here — FastAPI runs routes now
 
