3c7dde5f62c0f2a755c881492156c2aeefb4d8e3
mogesTesema <moges2421@gmail.com>
2025-11-28 03:22:21 +0300

    configer logging_config function to log system runtime log on storeapi.log file and sotre up to 1GB for 5 iteration

--- storeapi/logging_config.py
+++ storeapi/logging_config.py
@@ -10,6 +10,11 @@ def configure_logging()->None:
                     "class":"logging.Formatter",
                     "datefmt":"%Y-%m-%dT%H:%M:%S",
                     "format":"%(name)s:%(lineno)d - %(message)s"
+                },
+                "file":{
+                    "class":"logging.Formatter",
+                    "datefmt":"%Y-%m-%dT%H:%M:%S",
+                    "format":"%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s"
                 }
             },
             "handlers":{
@@ -17,12 +22,21 @@ def configure_logging()->None:
                     "class":"rich.logging.RichHandler",
                     "level":"DEBUG",
                     "formatter":"console"
+                },
+                "rotating_file":{
+                    "class":"logging.handlers.RotatingFileHandler",
+                    "level":"DEBUG",
+                    "formatter":"file",
+                    "filename":"storeapi.log",
+                    "maxBytes":2024 * 2024, # 1GB
+                    "backupCount":5,
+                    "encoding":"utf8"
 
                 }
             },
             "loggers":{
                 "uvicorn":{
-                    "handlers":["default"],
+                    "handlers":["default","rotating_file"],
                     "level":"INFO"
 
                 },
@@ -35,7 +49,7 @@ def configure_logging()->None:
                     "level":"CRITICAL"
                 },
                 "storeapi":{
-                    "handlers":["default"],
+                    "handlers":["default","rotating_file"],
                     "level":"DEBUG" if isinstance(config,DevConfig) else "INFO",
                     "propagate":False
                 }
