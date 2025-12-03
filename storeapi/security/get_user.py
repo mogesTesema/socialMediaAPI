8d62589fd3b903ae7a9e57d808c4004811d496eb
mogesTesema <moges2421@gmail.com>
2025-12-03 22:15:21 +0300

    decople create confirmation and access token,generic HTTPException handle with dynamic parameter

--- storeapi/security/get_user.py
+++ storeapi/security/get_user.py
@@ -7,7 +7,7 @@ import logging
 from jwt import ExpiredSignatureError, PyJWTError
 import jwt
 import datetime
-from typing import Annotated
+from typing import Annotated, Literal
 
 logger = logging.getLogger(__name__)
 
@@ -17,13 +17,22 @@ ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 password_hasher = PasswordHasher()
 
-credentials_exception = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED, detail="could not validate credentials"
-)
+
+def create_credentials_exception(
+    detail: str, status_code=status.HTTP_401_UNAUTHORIZED
+) -> HTTPException:
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=detail,
+    )
 
 
 def access_token_expire_minutes() -> int:
     return 30
+
+
+def confirm_token_expire_minutes() -> int:
+    return 5
 
 
 def create_access_token(email: str):
@@ -31,10 +40,52 @@ def create_access_token(email: str):
     expire = datetime.datetime.now(tz=datetime.UTC) + datetime.timedelta(
         minutes=access_token_expire_minutes()
     )
-    jwt_data = {"sub": email, "exp": expire}
+    jwt_data = {"sub": email, "exp": expire, "type": "access"}
     encoded_jwt = jwt.encode(payload=jwt_data, key=SECRETE_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def create_confirm_token(email: str):
+    logger.debug(f"create confirmation token for user:{email}")
+    confirm_expire = datetime.datetime.now(tz=datetime.UTC) + datetime.timedelta(
+        minutes=confirm_token_expire_minutes()
+    )
+
+    jwt_confirm_data = {"sub": email, "exp": confirm_expire, "type": "confirmation"}
+
+    encoded_confirm_token = jwt.encode(
+        payload=jwt_confirm_data, key=SECRETE_KEY, algorithm=ALGORITHM
+    )
+
+    return encoded_confirm_token
+
+
+def get_subject_token_type(token: str, type: Literal["access", "confirmation"]) -> str:
+    try:
+        payload = jwt.decode(jwt=token, key=SECRETE_KEY, algorithms=[ALGORITHM])
+
+    except ExpiredSignatureError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from e
+
+    except PyJWTError as e:
+        raise create_credentials_exception(detail="invalid token") from e
+
+    email = payload.get("sub")
+    token_type = payload.get("type")
+    if token_type != type or token_type is None:
+        raise create_credentials_exception(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail="token has incorrect type, expected: {token_type}",
+        )
+    if not email:
+        raise create_credentials_exception(detail="Token is missing 'sub' field")
+
+    return email
 
 
 async def decrpypt_access_token(access_token: str):
@@ -61,33 +112,20 @@ async def authenticate_user(email: str, password: str):
     logger.debug("Authenticaling user", extra={"email": email})
     user = await get_user(email=email)
     if not user:
-        raise credentials_exception
+        raise create_credentials_exception(detail="invalid email or password")
     if not verify_password(plain_password=password, hashed_password=user.password):
-        raise credentials_exception
+        raise create_credentials_exception("invalid email or password")
 
     return user
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    try:
-        payload = jwt.decode(jwt=token, key=SECRETE_KEY, algorithms=[ALGORITHM])
-
-        email = payload.get("sub")
-        if not email:
-            raise credentials_exception
-
-    except ExpiredSignatureError as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired",
-            headers={"WWW-Authenticate": "Bearer"},
-        ) from e
-
-    except PyJWTError as e:
-        raise credentials_exception from e
-
+    email = get_subject_token_type(token=token, type="access")
     user = await get_user(email=email)
 
     if not user:
-        raise credentials_exception
+        raise create_credentials_exception(
+            detail="user not found for this token",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
     return user
