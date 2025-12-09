452c6def10fc34dbc1175949bdeaaf3120b35216
mogesTesema <moges2421@gmail.com>
2025-12-09 23:06:04 +0300

    setting up conftest.py to set up pytest fixture

--- storeapi/tests/conftest.py
+++ storeapi/tests/conftest.py
@@ -1,31 +1,30 @@
-from typing import AsyncGenerator,Generator
+from typing import AsyncGenerator, Generator
+
 import pytest
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
-from storeapi.main import app
-from storeapi.routers.post import comment_table,post_table
 
+from storeapi.main import app
+from storeapi.routers.post import comment_table, post_table
 
 
 @pytest.fixture(scope="session")
 def anyio_backend():
     return "asyncio"
 
-@pytest.fixture()
-def client()->Generator:
-    yield TestClient(app)
+
+def client() -> Generator:
+    yield TestClient(app=app, base_url="http:127.0.0.1/8000")
 
 
 @pytest.fixture(autouse=True)
-async def db()->AsyncGenerator:
-    comment_table.clear()
+async def db() -> AsyncGenerator:
     post_table.clear()
+    comment_table.clear()
+    yield
 
 
 @pytest.fixture()
-async def async_client(client)->AsyncGenerator:
-    async with AsyncClient(app=app,base_url=client.base_url) as ac:
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url=client.base_url) as ac:
         yield ac
-
-
-
