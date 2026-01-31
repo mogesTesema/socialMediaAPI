6c7b06235b577a4283d0f06af0b0f1d22d2c1fb8
mogesTesema <moges2421@gmail.com>
2026-01-31 22:02:12 +0300

    Add food vision model folder and deps

---
+++ storeapi/food_vision_model/food_prediction.py
@@ -0,0 +1,5 @@
+"""
+I am going to load onnx food vision tensorflow model for food prediction.
+and I will create a function to predict food item from image.
+and return the predicted food item as a string.
+"""
